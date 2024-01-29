00f6f09002e002bf85d77a3fdeeedab17278ad73
9janinja <awure_barry@yahoo.com>
2024-01-29 18:22:35 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -32,21 +32,3 @@ variable "availability_zones" {
   type    = list(string)
   default = ["us-west-2a", "us-west-2b"]
 }
-# variable "kubernetes_ami" {
-#   default = "ami-02efe31f21526284a"
-# }
-
-# variable "medium_instance_type" {
-#   default = "t2.medium"
-# }
-
-# variable "micro_instance_type" {
-#   default = "t2.micro"
-# }
-
-# variable "private_key_path" {
-#   default = "Automationkey.pem"
-# }
-# variable "server_ami" {
-#   default = "ami-086d8b62958af8558"
-# }
