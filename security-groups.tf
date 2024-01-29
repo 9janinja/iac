01da18d560c80bd730cb6636551a39ffc107c1ea
9janinja <awure_barry@yahoo.com>
2024-01-29 17:08:18 +0100

    security group and application edit

--- security-groups.tf
+++ security-groups.tf
@@ -17,14 +17,6 @@ resource "aws_security_group" "duc_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
-  egress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   ingress {
     from_port   = 443
     to_port     = 443
@@ -33,10 +25,9 @@ resource "aws_security_group" "duc_sg" {
   }
 
   egress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
 }
